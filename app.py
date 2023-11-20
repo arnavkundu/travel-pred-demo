f1965a0a996647381a9e887db22c01f0ef5b04bb
Arnav Kundu <arnav.speadfast@gmail.com>
2023-11-20 22:36:23 +0530

    new changes to see build

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 
 # New changes 
+print("Adding some new changes to see the automatic build")
 
 # Load the models from the files
 loaded_lr_model = joblib.load('./model/lr_model.joblib')
