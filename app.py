a2151d2422b670f4540892d124d5bd9fd3d486d2
Arnav Kundu <arnav.speadfast@gmail.com>
2023-11-21 21:07:41 +0530

    new changes to see build Live

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 # New changes 
-print("Adding some new changes to see the automatic build Part 2")
+print("Adding some new changes to see the automatic build Part 2 - Live class")
 
 # Load the models from the files
 loaded_lr_model = joblib.load('./model/lr_model.joblib')
